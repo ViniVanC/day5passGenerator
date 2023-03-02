cd47b4f8ebd99a475fc06f97082f047872d56dab
ViniVanC <vincentvan0302@gmail.com>
2023-03-02 19:43:49 +0200

    commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "gradient-1":
+          "linear-gradient(148deg, rgba(1,72,114,1) 0%, rgba(160,234,207,1) 100%)",
+        "gradient-2": "linear-gradient(-45deg, #CC7952 0%, #474344 100%)",
+      }),
+    },
   },
   plugins: [],
 };
