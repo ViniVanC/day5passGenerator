d9bd1e3316e6f6b05541b4d4691dbc179d3c013f
ViniVanC <vincentvan0302@gmail.com>
2023-03-02 20:43:47 +0200

    commit

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -9,7 +9,7 @@ export const CheckBox = ({
   onCheckedItem,
 }) => {
   return (
-    <li className={`flex items-center w-1/2 mb-[10px] ${styles}`}>
+    <li className={`checkbox flex items-center w-1/2 mb-[10px] ${styles}`}>
       <input
         className="cursor-pointer mr-[10px]"
         type="checkbox"
@@ -18,11 +18,7 @@ export const CheckBox = ({
         checked={status}
         onChange={() => onCheckedItem(id)}
       />
-      <label
-        className="cursor-pointer"
-        htmlFor={id}
-        // onClick={() => onCheckedItem(id)}
-      >
+      <label className="cursor-pointer" htmlFor={id}>
         {text}
       </label>
     </li>
