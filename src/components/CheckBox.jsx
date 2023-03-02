5479fbab80e372d8d4c188984d7d566ef04a5cab
ViniVanC <vincentvan0302@gmail.com>
2023-03-02 15:55:17 +0200

    commit

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
-export const CheckBox = () => {
-  return <div>CheckBox</div>;
+export const CheckBox = ({ id, text, disabled, styles }) => {
+  return (
+    <li className={`flex items-center w-1/2 mb-[10px] ${styles}`}>
+      <input
+        className="cursor-pointer mr-[10px]"
+        type="checkbox"
+        id={id}
+        disabled={disabled}
+      />
+      <label className="cursor-pointer" htmlFor={id}>
+        {text}
+      </label>
+    </li>
+  );
 };
