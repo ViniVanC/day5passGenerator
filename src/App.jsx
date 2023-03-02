493475bfd9380680b0f09ee955a33ab8316863db
ViniVanC <vincentvan0302@gmail.com>
2023-03-02 15:55:04 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { Container } from "./components/Container";
+
+const characters = [
+  {
+    id: "lowercase",
+    text: "Lowercase (a-z)",
+    value: "abcdefghijklmnopqrstuvwxyz",
+    disabled: true,
+  },
+  {
+    id: "uppercase",
+    text: "Uppercase (A-Z)",
+    value: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    disabled: false,
+  },
+  {
+    id: "numbers",
+    text: "Numbers (0-9)",
+    value: "0123456789",
+    disabled: false,
+  },
+  {
+    id: "symbols",
+    text: "Symbols (!-$^+)",
+    value: "!$%&|[](){}:;.,*+-#@<>~",
+    disabled: false,
+  },
+];
+
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="h-[100vh] bg-[#474344] flex justify-center items-center ">
+      <Container characters={characters} />
+    </div>
+  );
 }
 
 export default App;
