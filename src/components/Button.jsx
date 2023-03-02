af689249637355ec8aa9cbb94d676a86efc87e58
ViniVanC <vincentvan0302@gmail.com>
2023-03-02 15:55:12 +0200

    commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-export const Button = ({ onClick, text, ...props }) => {
+export const Button = ({ onClick, text, styles }) => {
   return (
-    <button className=" uppercase" onClick={onClick}>
+    <button
+      className={`uppercase p-[10px] w-full bg-[#933d2c] text-[#dfd0bb] font-semibold rounded-[10px] hover:bg-[#682b1e] transition duration-300 ${styles}`}
+      onClick={onClick}
+    >
       {text}
     </button>
   );
